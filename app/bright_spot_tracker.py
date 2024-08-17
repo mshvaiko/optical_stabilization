3f3b9473d7f295ad8e19d674d6fa39258969358c
sashapvt <sashapvt@gmail.com>
2024-08-18 00:37:09 +0300

    Add posibility of demontration results of multiple algorytms. Separate original and debug onscreen image. Provide more examples

--- app/bright_spot_tracker.py
+++ app/bright_spot_tracker.py
@@ -9,7 +9,7 @@ UPPER_RED1 = np.array([147, 255, 255])
 LOWER_RED2 = np.array([150, 100, 100])
 UPPER_RED2 = np.array([180, 255, 255])
 
-def process_frame(frame):
+def process_frame(frame, osd_frame):
     # Convert the frame to HSV color space
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     
@@ -45,18 +45,18 @@ def process_frame(frame):
             coordinates = (cX_centered, cY_centered)
             
             # Print the coordinates on the frame
-            cv2.circle(frame, (cX, cY), 1, (0, 0, 255), -1)
-            cv2.putText(frame, f"({cX_centered}, {cY_centered})", (cX + 10, cY + 10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+            cv2.circle(osd_frame, (cX, cY), 1, (0, 0, 255), -1)
+            cv2.putText(osd_frame, f"({cX_centered}, {cY_centered})", (cX + 10, cY - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
     # Print level colors
     lower_red1_bgr = cv2.cvtColor(np.uint8([[LOWER_RED1]]), cv2.COLOR_HSV2BGR)[0][0]
-    cv2.rectangle(frame, (0,0), (50,50), lower_red1_bgr.tolist(), -1)
+    cv2.rectangle(osd_frame, (0,0), (50,50), lower_red1_bgr.tolist(), -1)
     upper_red1_bgr = cv2.cvtColor(np.uint8([[UPPER_RED1]]), cv2.COLOR_HSV2BGR)[0][0]
-    cv2.rectangle(frame, (50,0), (100,50), upper_red1_bgr.tolist(), -1)
+    cv2.rectangle(osd_frame, (50,0), (100,50), upper_red1_bgr.tolist(), -1)
     lower_red2_bgr = cv2.cvtColor(np.uint8([[LOWER_RED2]]), cv2.COLOR_HSV2BGR)[0][0]
-    cv2.rectangle(frame, (0,50), (50,100), lower_red2_bgr.tolist(), -1)
+    cv2.rectangle(osd_frame, (0,50), (50,100), lower_red2_bgr.tolist(), -1)
     upper_red2_bgr = cv2.cvtColor(np.uint8([[UPPER_RED2]]), cv2.COLOR_HSV2BGR)[0][0]
-    cv2.rectangle(frame, (50,50), (100,100), upper_red2_bgr.tolist(), -1)
+    cv2.rectangle(osd_frame, (50,50), (100,100), upper_red2_bgr.tolist(), -1)
 
     return coordinates
--- app/main.py
+++ app/main.py
@@ -7,14 +7,36 @@ import bright_spot_tracker as bst
 import orb_tracker as ot
 
 # Define the video file path
-# VIDEO_PATH = r'./assets/moving_red_spot.mp4' # for bright_spot_tracker
-VIDEO_PATH = r'./assets/flying_drone.mov' # orb_tracker
+# VIDEO_PATH = r'./assets/moving_red_spot.mp4' # bright_spot_tracker
+# VIDEO_PATH = r'./assets/flying_drone.mov' # orb_tracker
+VIDEO_PATH = r'./assets/red_drone_in_the_sky_from_start.mp4' # bright_spot_tracker, orb_tracker
+# VIDEO_PATH = r'./assets/red_drone_in_the_sky_bird.mp4' # bright_spot_tracker, orb_tracker
 
 # FOR DEBUG: Use playback delay to match the video frame rate
 DEBUG_FRAME_DELAY = True
 
-# Define the working algorytm
-WORKING_ALGORYTM = 'ot' # 'bst', 'ot'
+def dispay_image(frame, osd_frame, title, max_width=1920, max_height=1080):
+    # Print the cross and misc text
+    cv2.line(osd_frame, (osd_frame.shape[1] // 2, 0), (osd_frame.shape[1] // 2, osd_frame.shape[0]), (0, 255, 0), 1)
+    cv2.line(osd_frame, (0, osd_frame.shape[0] // 2), (osd_frame.shape[1], osd_frame.shape[0] // 2), (0, 255, 0), 1)
+    cv2.putText(osd_frame, f"Press 'q' to exit", (0, 120),
+    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+
+    # Blend the osd_frame with the frame
+    merged_frame = cv2.addWeighted(frame, 1, osd_frame, 1, 0)
+
+    # Get the current frame dimensions
+    height, width = merged_frame.shape[:2]
+
+    # Check if resizing is needed
+    if width > max_width or height > max_height:
+        # Calculate the scaling factor
+        scaling_factor = min(max_width / width, max_height / height)
+        # Resize the frame
+        merged_frame = cv2.resize(merged_frame, (int(width * scaling_factor), int(height * scaling_factor)))
+
+    # Display the frame
+    cv2.imshow(title, merged_frame)
 
 def main():
     # Open the video file using cv2.VideoCapture
@@ -30,28 +52,28 @@ def main():
     frame_delay = 1 / fps
 
     while True:
-        # Capture frame-by-frame
+        # Capture frame
         ret, frame = cap.read()
 
         if not ret:
             break
 
-        # Process the frame to find and track the drone
-        match WORKING_ALGORYTM:
-            case 'bst':
-                coordinates = bst.process_frame(frame)
-            case 'ot':
-                coordinates = ot.process_frame(frame)
+        bst_coordinates, ot_coordinates = None, None
+
+        # Create a frame to display the OSD (debug info)
+        osd_frame = np.zeros(frame.shape, dtype=np.uint8)
+
+        # Use the bright spot tracker module
+        bst_coordinates = bst.process_frame(frame, osd_frame)
+
+        # Use the ORB tracker module
+        ot_coordinates = ot.process_frame(frame, osd_frame)
+
+        # Use the coordinates from the module that detected the drone
+        coordinates = bst_coordinates or ot_coordinates
         print(f"Drone coordinates: {coordinates}")
 
-        # Print the cross and misc text
-        cv2.line(frame, (frame.shape[1] // 2, 0), (frame.shape[1] // 2, frame.shape[0]), (0, 255, 0), 1)
-        cv2.line(frame, (0, frame.shape[0] // 2), (frame.shape[1], frame.shape[0] // 2), (0, 255, 0), 1)
-        cv2.putText(frame, f"Press 'q' to exit", (0, 120),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
-
-        # Display the resulting frame
-        cv2.imshow('Tracking Drone', frame)
+        dispay_image(frame, osd_frame, 'Tracking Drone')
 
         # Introduce a delay to match the frame rate of the video
         if DEBUG_FRAME_DELAY:
